57d3e565cff36fc48a5850281a8e8ec14e883b72
Icebob <mereg.norbert@gmail.com>
2011-11-10 14:29:10 +0000

    test commit

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 //	$GLOBALS["root_directory"]  = dirname(__FILE__) . DS . "../";
 	$GLOBALS["root_directory"]  = dirname(__FILE__) . DS . "test";
 	$GLOBALS["web_root"]		= "http://icedit.sourceforge.net/icedit-code/test";
-	
+	 
 	$GLOBALS["default_permission"] = 0775;
 	$GLOBALS["default_owner"] = "iceapps";
 	
